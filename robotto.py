4c9e4bbcf91e96ca4e8808c5f7d2d0a235016e69
sectrimte <sectrimte@gmail.com>
2019-01-18 01:00:17 +0100

    COMMIT State machine
    Added State Machine class
    Updated move function (long press of 0.2 sec to register)

--- robotto.py
+++ robotto.py
@@ -1,9 +1,11 @@
 import pyautogui
 import random
-
+from state import State
+import time
+    
 fight_screen_bg_color = [52, 52, 52]
-fight_screen_blank_xy = []
-button_run_xy = []
+fight_screen_blank_xy = [533,733]
+button_run_xy = [486, 755]
 
 def path_circle_by_sizes(height, length):
     path = ['right']*length + ['down']*height + ['left']*length + ['up']*height
@@ -23,22 +25,79 @@ def path_rand_by_sizes(height, length, current_position=[0, 0]):
     return path
 
 def move(path):
-    pyautogui.typewrite(path, 0.25)
+    #pyautogui.typewrite(path, 0.5)
+    for key in path:
+        hold_key(key, 0.2)
+        print('pressed '+key+'\n')
 
-move(path_circle_by_sizes(3, 3))
-move(path_rand_by_sizes(3, 3))
+def automove(h, l):
+    move(path_circle_by_sizes(h, l))
+    move(path_rand_by_sizes(h, l))
+
+def hold_key(key, hold_time):
+    start = time.time()
+    pyautogui.keyDown(key)
+    while time.time() - start < hold_time:
+        pass
+    pyautogui.keyUp(key)
     
+#definition state machine
+class MoveState(State):
+    def on_event(self, event):
+        if event == 'encounter':
+            return EncounterState()
+
+        return self
+        
+class EncounterState(State):
+    def on_event(self, event):
+        if event == 'pokemon_ok':
+            return FightingState()
+        if event == 'pokemon_ko':
+            return MoveState()
+
+        return self
+
+class FightingState(State):
+    def on_event(self, event):
+        if event == 'encounter':
+            return UnlockedState()
+
+        return self
+
+class WaitingState(State):
+    def on_event(self, event):
+        if event == 'encounter':
+            return UnlockedState()
+
+        return self
+
+class LearningState(State):
+    def on_event(self, event):
+        if event == 'encounter':
+            return UnlockedState()
+
+        return self
+
+states = ['move', 'encounter', 'check_pokemon', 'run']
+
+current_state = MoveState()
+#move(path_circle_by_sizes(4, 4))
+#exit()
+
 try:
     while True:
         #get mouse coordinates
-        x, y = pyautogui.position()
-        
+        x, y = pyautogui.position()        
         #screenshot and get pixel color under the mouse cursor
         pixelColor = pyautogui.screenshot().getpixel((x, y))
         
+        if repr(current_state) == 'MoveState':
+            
+        
 except KeyboardInterrupt:
     print('\nDone')
-    
+        
 
 
     
